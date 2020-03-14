9e41787b832217ba02701356e06e7a9d2ddaf252
Nicholas Car <nicholas.car@csiro.au>
2020-03-14 21:34:33 +1000

    black-ed all code files

--- src/pylode/server.py
+++ src/pylode/server.py
@@ -5,13 +5,13 @@ import subprocess
 class DocResource:
     def on_get(self, req, resp):
         """Handles GET requests"""
-        url = req.get_param('url')
+        url = req.get_param("url")
         if url is not None:
             cmd = "cd ./bin && ./pylode -u {url} -c true".format(url=url)
             subprocess.call(cmd, shell=True)
-            resp.set_header('Powered-By', 'Falcon')
+            resp.set_header("Powered-By", "Falcon")
             resp.status = falcon.HTTP_200
 
 
 api = falcon.API()
-api.add_route('/lode', DocResource())
+api.add_route("/lode", DocResource())
