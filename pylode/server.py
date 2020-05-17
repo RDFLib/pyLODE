24504673ca75a6481b59fee6a74ab49d1a1553ab
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-05-17 23:13:53 +1000

    remove Overview sec from HTML if served by Falcon (Issue 72)

--- pylode/server.py
+++ pylode/server.py
@@ -8,7 +8,13 @@ class DocResource:
         url = req.get_param("url")
         if url is not None:
             cmd = "cd ./bin && ./pylode.sh -u {url} -c true".format(url=url)
-            resp.body = subprocess.check_output(cmd, shell=True)
+
+            # remove Overview image placeholder, if present
+            unprocessed_html = subprocess.check_output(cmd, shell=True)
+            unprocessed_html = unprocessed_html.decode().replace(
+                '<section id="overview">', '<section id="overview" style="display:none;">').encode()
+            resp.body = unprocessed_html
+
             resp.set_header("Powered-By", "Falcon")
             resp.set_header("content-type", "text/html")
             resp.status = falcon.HTTP_200
