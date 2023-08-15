a9331fc83a64ab435c51a2a09fffbfa6da443f3c
Edmond Chuc <edmond@kurrawong.ai>
2023-08-15 16:09:26 +1000

    Add styling to external_link to ensure icon is on same line

--- pylode/profiles/supermodel/component/link.py
+++ pylode/profiles/supermodel/component/link.py
@@ -4,9 +4,7 @@ from dominate.tags import a, i
 def external_link(value: str, href: str) -> a:
     """An anchor tag with an external icon with the target attribute set to _blank."""
     with a(
-        value,
-        href=href,
-        target="_blank",
+        value, href=href, target="_blank", style="white-space: nowrap;"
     ) as component:
         i(
             _class="fa fa-external-link",
