c89de911514df094ad9e14c7e0e7ab9ee9d6a8f4
David Linke <david.linke@catalysis.de>
2023-08-31 19:54:43 +0200

    Update to modern python packaging
    and using hatch as build tool and hatch-vcs
    for auto versioning based on git tags.

--- pylode/__init__.py
+++ pylode/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "2.13.2"
+try:
+    from pylode._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
 
 from .common import *
 from .profiles import OntDoc, Prof, VocPub, NMPF, PROFILES, RDF_MEDIA_TYPES
