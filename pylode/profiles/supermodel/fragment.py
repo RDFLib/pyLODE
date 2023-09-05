65d3e4a128038b101ec1d10917fa79a767da8031
Edmond Chuc <edmond@kurrawong.ai>
2023-09-05 18:46:08 +1000

    HTML fragment characters are no longer lowered

--- pylode/profiles/supermodel/fragment.py
+++ pylode/profiles/supermodel/fragment.py
@@ -4,4 +4,4 @@ from pylode.profiles.supermodel.random import random_id
 def make_html_fragment(value: str | None):
     if value is None or value == "":
         return random_id()
-    return value.lower().replace(" ", "-")
+    return value.replace(" ", "-")
