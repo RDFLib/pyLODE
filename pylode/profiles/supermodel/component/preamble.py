f8676da25f331f9bd64c26b85fcec8b0a37fa506
Edmond Chuc <edmond@kurrawong.ai>
2023-09-05 18:46:37 +1000

    Add docstring to metadata_row component

--- pylode/profiles/supermodel/component/preamble.py
+++ pylode/profiles/supermodel/component/preamble.py
@@ -3,6 +3,12 @@ from dominate.tags import tr, td, p, a, strong
 
 @tr
 def metadata_row(key: str, value: str | list[str], value_is_link: bool = False) -> td:
+    """Create a metadata table row in the preface.
+
+    :param key: the key in the key-value pair
+    :param value: the value in the key-value pair
+    :param value_is_link: whether this is rendered as an HTML anchor tag or not
+    """
     with td(
         _class="tableblock halign-right valign-top",
         style="background-color: #FFFFFF;",
